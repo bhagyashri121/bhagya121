04b6e513dace492787c72ce75b8cf98a7559852e
unknown <32457@EBI080F2EW.corp.hexaware.com>
2016-03-17 18:38:56 +0530

    jP

---
+++ JProject/src/jproject/JProject.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jproject;
+
+/**
+ *
+ * @author 32457
+ */
+public class JProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    static int i=9;
+    public static void main(String[] args) {
+        int j =0 ;
+        System.out.println(i);// TODO code application logic here
+    }
+    
+}
